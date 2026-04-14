aabb7e2c23295b11827841dd3f77b544fc078f0c
Vaishnavi Newalkar <vaishnavinewalkar11@gmail.com>
2026-04-14 20:23:12 +0000

    commit1

---
+++ 01_baiscs/01_variables.js
@@ -0,0 +1,10 @@
+const accountID = 123443
+let accountEmail = "vaishn@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountID = 2
+accountEmail = "holaa@gmail.com"
+console.log(accountID)
+
+console.table([accountEmail, accountID,accountPassword, accountCity])
