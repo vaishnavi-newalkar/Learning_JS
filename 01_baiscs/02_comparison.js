2bc83bc01d9890fbab0f2b85efa770ab8f0a4505
Vaishnavi Newalkar <vaishnavinewalkar11@gmail.com>
2026-04-27 05:46:58 +0000

    Add initial comparison and data types examples

---
+++ 01_baiscs/02_comparison.js
@@ -0,0 +1,7 @@
+// console.log(2>1);
+// console.log(2!=1);
+
+console.log("2">1);  //data type same hona chahiye for comparison
+//Above comparison result is unpredictable
+
+
---
+++ 01_baiscs/03_DataTypes.js
@@ -0,0 +1,22 @@
+// # Primitive Data Type
+// 7 categories-> string , NUmber, Boolean, null(khaali/empty), undefined, Symbol
+//BigInt
+
+// Reference Data type(non primitive)
+//Array , Onjects, Functions
+
+// const score = 100;
+// const score = false;
+
+//Check if JAVASCRIPT is statistically typed/ Dynamically typed
+const heros = ["Iron man","Captian America"]
+let myObj = {
+    name: "Vaishnavi",
+    age: 22,
+}
+
+const myFunction= function(){
+    console.log("Hello world\n");
+}
+
+console.log(typeof(myFunction))
